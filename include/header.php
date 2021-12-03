86ced7ac3012bdb0d33e8f1d8af6579f9ec87d10
Alex-Zicaro <alex.zicaro@laplateforme.io>
2021-12-03 15:03:33 +0100

    headerpasfinis

--- include/header.php
+++ include/header.php
@@ -1,8 +1,3 @@
-<head>
-    <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-
 <header class="container-fluid-xl  sticky-top" id=header>
 
     <nav class="h-100 navbar navbar-expand-md navbar-dark bg-dark ">
@@ -19,24 +14,16 @@
 
             <ul class="bg-dark justify-content-around navbar-nav ">
 
-                <p class='d-flex align-items-center text-light my-2 bg-dark text-nowrap align ml-5'>Bienvenue à toi inconnue </p>
+                <p class='d-flex align-items-center text-light my-2 bg-dark text-nowrap align ml-5'>Bienvenue à toi </p>
 
-                <li class="d-flex mb-3 ">
-                    <a class="btn btn-light" href="inscription.php">Inscription</a>
+                <li class="nav-item ml-5 my-2 ">
+                    <a class="btn btn-light" href="profil.php">modifier votre profil</a>
                 </li>
-                <li class="d-flex mb-3">
-                    <a class="btn btn-light" href="connexion.php">Connexion</a>
+                <li class="nav-item ml-5 my-2">
+                    <a class="btn btn-light" href="livre-or.php">Livre d'or</a>
                 </li>
 
             </ul>
-
-            <select >
-
-                <option>Catégorie 1 </option>
-                <option>Catégorie 2</option>
-                <option>Catégorie 3</option>
-
-            </select>
 
         </div>
 
