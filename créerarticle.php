ace4df159779ba89abf2cdd75485f2a78f203afe
Marc Keil <marc.keil@laplateforme.io>
2021-12-06 15:48:07 +0100

    avancement créer article

--- créerarticle.php
+++ créerarticle.php
@@ -2,26 +2,31 @@
 require('config/bdd.php');
 session_start();
 
-if(!isset($_SESSION['id']) ||  $_SESSION['id'] != 42 || $_SESSION['id'] != 1337 ) // ID a changer a modérateur et admin
+ if(!isset($_SESSION['id'])) // ID a changer a modérateur et admin
 {
     exit();
 }
 else 
 {
+
+
     $getid = intval($_SESSION['id']); // Convertie ma valeur en int ( ID = un numéro )
     $requtilisateur = $bdd->prepare('SELECT * FROM utilisateurs WHERE id = ?'); // créer une requete qui va récuperer tout de mon utilisateur de mon id actuel
     $requtilisateur->execute(array($getid)); // return le tableau de mon utilisateur
     $infoutilisateur = $requtilisateur->fetch(); // récupere les informations que j'appelle
-    $c_msg = "";
+    $a_msg = "";
 
     if(isset($_POST['submit_article'])) 
     {
-        
+            
+    $titre = htmlspecialchars($_POST['titre']);
+    $article = htmlspecialchars($_POST['article']);
+    
         if(isset($_POST['article']) && !empty($_POST['article']))
         {
             $articlelenght = strlen($_POST['article']);
 
-            if($comlenght > 5000)
+            if($articlelenght > 5000)
             $a_msg = "Votre article ne doit pas dépasser 5000 caractères !<br><br>";
 
         if ($a_msg == "") {
@@ -64,8 +69,8 @@ else
 <main>
     <form method="POST">
         Votre pseudo : <?php echo $infoutilisateur['login'] ?><br><br>
-        <input type="text" placeholder="Votre login" name="login" id="login" value="<?php if(isset($login)) { echo $login; } ?>" >
-        <textarea name="article" placeholder="Votre article..." style="width: 300px; height: 100px"></textarea><br /><br>
+        <input type="text" placeholder="Titre" name="titre" id="titre" value="<?php if(isset($titre)) { echo $titre; } ?>" ><br><br>
+        <textarea name="article" placeholder="Votre article..." value="<?php if(isset($article)) { echo $article; } ?>" style="width: 300px; height: 100px"></textarea><br /><br>
         <input type="submit" value="Poster mon article" name="submit_article"/>
     </form>
 <br>
