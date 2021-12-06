b7bc3a0cb3216624305513bfa0e94cdd947995cb
Alex-Zicaro <alex.zicaro@laplateforme.io>
2021-12-06 12:36:12 +0100

    headerV1

--- include/header.php
+++ include/header.php
@@ -1,37 +1,37 @@
-<header class="container-fluid-xl  sticky-top" id=header>
+<!DOCTYPE html>
+<html>
 
-    <nav class="h-100 navbar navbar-expand-md navbar-dark bg-dark ">
+<head>
+    <title></title>
+    <meta charset='utf-8'>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
 
-        <a class="navbar-brand h-100" href="index.php"><img src="images/livreor.jpg" width="20%" class="h-100 img-fluid" alt="logo">Accueil</a>
+<body>
+    <div id="fondHead">
+        <div id="" class="container">
+            <h1>Le plus beau des blogs</h1>
+            <nav>
+                <ul class="nav nav-tabs">
+                    <li class="nav-item"><a class="nav-link" href="#">Accueil</a></li>
+                    <li>
+                        <div class="container">
+                            <div class="dropdown">
+                                <button class="dropbtn">Articles</button>
+                                <div class="dropdown-content">
 
-        <button class=" navbar-toggler navbar-toggler-dark bg-dark" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-
-            <ul class="bg-dark justify-content-around navbar-nav ">
-
-                <p class='d-flex align-items-center text-light my-2 bg-dark text-nowrap align ml-5'>Bienvenue à toi </p>
-
-                <li class="nav-item ml-5 my-2 ">
-                    <a class="btn btn-light" href="profil.php">modifier votre profil</a>
-                </li>
-                <li class="nav-item ml-5 my-2">
-                    <a class="btn btn-light" href="livre-or.php">Livre d'or</a>
-                </li>
-
-            </ul>
-
+                                    <a href="#">Catégorie 1</a>
+                                    <a href="#">Catégorie 2</a>
+                                    <a href="#">Catégorie 3</a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="#">Inscription</a></li>
+                    <li class="nav-item"><a class="nav-link " href="#">Connexion</a></li>
+                </ul>
+            </nav>
         </div>
+    </div>
+</body>
 
-    </nav>
-
-</header>
-
-<?php
-
-
-?>
+</html>
