8be77670e3363e249b54a0e33b8348749790d3ad
rodriguez <claude.rodriguez@laplateforme.io>
2021-12-06 11:57:44 +0100

    footerversion1

--- include/footer.php
+++ include/footer.php
@@ -1,29 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+    
+    <div id="crfootdiv1">
+        <div class="crfootdiv2">
+            <ul class="crfootul">
+                <li><h2 class="crfooth2">Nous Suivre</h2></li>
+                <li class="crfootli"><a class="crfoota" href="#"><img class="crimgfoot1" src="../images/logofacebook.jpg" alt="facebook"></a></li>
+                <li class="crfootli"><a class="crfoota"  href="#"><img class="crimgfoot1"  src="../images/instagram.png" alt="instagram"></a></li>
+                <li class="crfootli"><a class="crfoota"  href="#"><img class="crimgfoot2" src="../images/twitter.png" alt="twitter"></a></li>
+                <li class="crfootli"><a class="crfoota"  href="https://github.com/marc-keil/blog"><img class="crimgfoot2"  src="../images/github.png" alt="Git Hub"></a></li>
+            </ul>
+        </div>
+                
+        <div class="crfootdiv2">
+            <ul class="crfootul">
+                <li><h2 class="crfooth2">Nous Suivre</h2></li>
+                <li class="crfootli"><a class="crfoota"  href="#">label</a></li>
+                <li class="crfootli"><a class="crfoota"  href="#">devellopeur</a></li>
+                <li class="crfootli"><a class="crfoota"  href="#">presse</a></li>
+            </ul>
+        </div>
 
-<div class="foot1">
-                <ul>
-                    <li><h2 class="footh2">Musique du monde</h2></li>
-                    <li><img src="../images/logofacebook.jpg" alt=""><a href="#">facebook</a></li>
-                    <li><img src="../images/instagram.png" alt=""><a href="#">instagram</a></li>
-                    <li><img src="../images/twitter.png" alt=""><a href="#">twitter</a></li>
-                    <li><img src="../images/github.png" alt=""><a href="https://github.com/marc-keil/blog">Git Hub</a></li>
-                </ul>
-            </div>
-            
-            <div class="foot4">
-                <ul>
-                    <li><h2 class="footh2">Qui Sommes Nous</h2></li>
-                    <li><a href="#">label</a></li>
-                    <li><a href="#">devellopeur</a></li>
-                    <li><a href="#">presse</a></li>
-                </ul>
-            </div>
-            <div class="foot1">
-                <ul>
-                    <li><h2 class="footh2">Légal</h2></li>
-                    <li><a href="#">mentions légales</a></li>
-                    <li><a href="#">conditions générales d'utilisation</a></li>
-                    <li><a href="#">politique de protection de données</a></li>
-                    <li><a href="#">cookies</a></li>
-                    <li><a href="#">carrières</a></li>
-                </ul>
-            </div>
+        <div class="crfootdiv2">
+            <ul class="crfootul">
+                <li><h2 class="crfooth2">Plan Du Site</h2></li>
+                <li class="crfootli"><a class="crfoota" href="admin.php">Admin</a></li>
+                <li class="crfootli"><a class="crfoota" href="article.php">Article</a></li>
+                <li class="crfootli"><a class="crfoota" href="connexion.php">connexion</a></li>
+                <li class="crfootli"><a class="crfoota" href="Créerarticle.php">Créerarticle</a></li>
+                <li class="crfootli"><a class="crfoota" href="editionprofil.php">editionprofil</a></li>
+                <li class="crfootli"><a class="crfoota" href="index.php">Index</a></li>
+                <li class="crfootli"><a class="crfoota" href="inscription.php">Inscription</a></li>
+                <li class="crfootli"><a class="crfoota" href="profil.php">Profil</a></li>
+            </ul>
+        </div>
+    </div>
+    </html>
