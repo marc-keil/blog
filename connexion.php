899142823e7b60b3f5695842ddbaf7b49c7e3bf0
Alex-Zicaro <alex.zicaro@laplateforme.io>
2021-12-06 13:59:13 +0100

    modifMain

--- connexion.php
+++ connexion.php
@@ -51,13 +51,13 @@ if(isset($_POST['formconnexion']))
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>Connexion</title>
 </head>
     <body>
     <header>
             <?php if (isset($_SESSION['login'])) {
-                    include_once("include/headeronline.php");
+                    header('location : profil.php');
                 } 
                 else{
                     include_once('include/header.php'); 
