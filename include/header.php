1ba85812aaaf001455f7fd3eab090ed716191742
Alex-Zicaro <alex.zicaro@laplateforme.io>
2021-12-06 15:29:31 +0100

    FrontHeader

--- include/header.php
+++ include/header.php
@@ -4,7 +4,7 @@
 <head>
     <title></title>
     <meta charset='utf-8'>
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
@@ -12,13 +12,13 @@
 <body>
     <div id="fondHead">
         <div id="" class="container">
-            <h1>Le plus beau des blogs</h1>
-            <nav>
-                <ul class="nav nav-tabs">
-                    <li class="nav-item"><a class="nav-link" href="index.php">Accueil</a></li>
-                    <li>
+            <h1 class="text-light">Le plus beau des blogs</h1>
+            <nav class="alflex">
+                <tr class="nav nav-tabs">
+                    <td class="nav-item"><a class="nav-link" href="index.php">Accueil</a></td>
+                    <td>
                             <div class="dropdown">
-                                <a class="">Articles</a>
+                                <a class="nav-link">Articles</a>
                                 <div class="dropdown-content">
 
                                     <a href="#">Catégorie 1</a>
@@ -26,10 +26,10 @@
                                     <a href="#">Catégorie 3</a>
                                 </div>
                             </div>
-                    </li>
-                    <li class="nav-item"><a class="nav-link" href="inscription.php">Inscription</a></li>
-                    <li class="nav-item"><a class="btn btn-primary " href="connexion.php">Connexion</a></li>
-                </ul>
+</td>
+                    <td class="nav-item"><a class="nav-link" href="inscription.php">Inscription</a></td>
+                    <td class="nav-item"><a class="btn btn-primary " href="connexion.php">Connexion</a></td>
+                </tr>
             </nav>
         </div>
     </div>
