c3fd4c8a02b0ae026559a560b8d2a674eac70d33
Alex-Zicaro <alex.zicaro@laplateforme.io>
2021-12-07 10:49:38 +0100

    modifBdd

--- config/bdd.php
+++ config/bdd.php
@@ -1,6 +1,14 @@
 <?php
 function connexion(){
-    return $bdd = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
+    
+    try {
+        $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+        $bdd ->setAttribute(PDO::ATTR_ERRMODE ,PDO::ERRMODE_WARNING);
+        return $bdd;
+        } 
+    catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+}
 }
 $bdd = connexion();
 ?>
