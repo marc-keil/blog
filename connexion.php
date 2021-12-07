ecea7b338ceeb6621c46d05029a0580fcd5cf5ec
rodriguez <claude.rodriguez@laplateforme.io>
2021-12-07 16:36:38 +0100

    frontmain

--- connexion.php
+++ connexion.php
@@ -2,43 +2,34 @@
 require('config/bdd.php');
 session_start();
 
-        if(isset($_SESSION['id']) && $_SESSION['id'] > 0)
-        {
-            header("Location: profil.php");
-        }
+if (isset($_SESSION['id']) && $_SESSION['id'] > 0) {
+    header("Location: profil.php");
+}
 
-if(isset($_POST['formconnexion']))
-{
+if (isset($_POST['formconnexion'])) {
     $loginconnect = htmlspecialchars($_POST['loginconnect']);
     $passwordconnect = $_POST['passwordconnect'];
     // password_hash($_POST['passwordconnect'], PASSWORD_BCRYPT);
-    if(!empty($loginconnect) AND !empty($passwordconnect))
-        {
-            $requeteutilisateur = $bdd->prepare("SELECT * FROM utilisateurs WHERE login = ?"); // SAVOIR SI LE MEME LOGIN EST PRIS
-            $requeteutilisateur->execute(array($loginconnect));   // Execute le prepare
-            $result = $requeteutilisateur->fetchAll();   // Return TOUTE la requete ( tableau )
-            if (count($result) > 0){ // S'il trouve pas de même login, il return mauvais login
-                $sqlPassword = $result[0]['password'];  // Récupere le resultat du tableau (0)  /!\ SI PAS LE 0 ça marche pas /!\ et la colonne password
-                if(password_verify($passwordconnect, $sqlPassword)) // Si passwordconnect est hashé et qu'il est pareil que sql password c'est bon 
-                    {
-                    $_SESSION['id'] = $result[0]['id'];
-                    $_SESSION['login'] = $result[0]['login'];
-                    $_SESSION['email'] = $result[0]['email'];
-                    header("Location: profil.php");
-                    
-                    }
-                else 
-                    {
-                    $erreur = "Mauvais mot de passe !";
-                    }
+    if (!empty($loginconnect) and !empty($passwordconnect)) {
+        $requeteutilisateur = $bdd->prepare("SELECT * FROM utilisateurs WHERE login = ?"); // SAVOIR SI LE MEME LOGIN EST PRIS
+        $requeteutilisateur->execute(array($loginconnect));   // Execute le prepare
+        $result = $requeteutilisateur->fetchAll();   // Return TOUTE la requete ( tableau )
+        if (count($result) > 0) { // S'il trouve pas de même login, il return mauvais login
+            $sqlPassword = $result[0]['password'];  // Récupere le resultat du tableau (0)  /!\ SI PAS LE 0 ça marche pas /!\ et la colonne password
+            if (password_verify($passwordconnect, $sqlPassword)) // Si passwordconnect est hashé et qu'il est pareil que sql password c'est bon 
+            {
+                $_SESSION['id'] = $result[0]['id'];
+                $_SESSION['login'] = $result[0]['login'];
+                $_SESSION['email'] = $result[0]['email'];
+                header("Location: profil.php");
+            } else {
+                $erreur = "Mauvais mot de passe !";
             }
-            else
-                $erreur = "Mauvais login !";
-        }
-    else
-        {
-         $erreur = "Tous les champs doivent être remplis !";
-        }
+        } else
+            $erreur = "Mauvais login !";
+    } else {
+        $erreur = "Tous les champs doivent être remplis !";
+    }
 }
 
 
@@ -47,6 +38,7 @@ if(isset($_POST['formconnexion']))
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -54,40 +46,65 @@ if(isset($_POST['formconnexion']))
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>Connexion</title>
 </head>
-    <body>
-    <header>
-            <?php if (isset($_SESSION['login'])) {
-                    header('location : profil.php');
-                } 
-                else{
-                    include_once('include/header.php'); 
-                }
-                ?>
-    </header>
 
-<main>
-            <h2>Connexion</h2>
-            <br /><br />
-            <form method="POST" action="" class="patate">
-            <label for="loginconnect">Login :</label><br>
-            <input type="text" name="loginconnect" placeholder="Login"><BR><BR>
-            <label for="password">Password :</label><br>
-            <input type="password" name="passwordconnect" placeholder="Password">
-            <br /><br />
-            <input type="submit" name="formconnexion" class="formconnexion" value="Se connecter !"><br><BR><br>
-        </form>
-        <?php 
-        if(isset($erreur))
-        {
-        echo '<font color="red">'.$erreur.'</font><br><br>';
+<body>
+    <header>
+        <?php if (isset($_SESSION['login'])) {
+            header('location : profil.php');
+        } else {
+            include_once('include/header.php');
         }
         ?>
-</main>
-<footer>
+    </header>
+
+    <main>
+        <br /><br />
+        <div id="crdivconnexion">
+            <div id="crh2connexion">
+                <h2 class="text-light">Connexion</h2>
+            </div>
+         
+            <form method="POST" action="" id="crforconnexion">
+                <table>
+                    <tr>
+                        <td class="crtdco">
+                            <label for="loginconnect">Login :</label>
+                        </td>
+                        <td class="crtdco">
+                            <input type="text" name="loginconnect" placeholder="Votre Login"> 
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="crtdco">
+                            <label for="password">Password :</label>
+                        </td>
+                        <td class="crtdco">
+                            <input type="password" name="passwordconnect" placeholder="Votre Password">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="crtdco">                           
+                           <input class="btn btn-primary" type="submit" name="formconnexion" id="crinputco" value="Se connecter !">
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+
+        
+                
+                
         <?php
-        include_once('include/footer.php'); 
+        if (isset($erreur)) {
+            echo '<font color="red">' . $erreur . '</font><br><br>';
+        }
         ?>
-</footer>
-    
+    </main>
+    <footer>
+        <?php
+        include_once('include/footer.php');
+        ?>
+    </footer>
+
 
 </html>
--- include/footer.php
+++ include/footer.php
@@ -7,7 +7,7 @@
     <div id="crfootdiv1">
         <div class="crfootdiv2">
             <ul class="crfootul">
-                <li><h2 class="crfooth2">Nous Suivre</h2></li>
+                <li><h2 class="crfooth2">Nous Suivre</h2></li> <hr class="crfooterhr">
                 <li class="crfootli"><a class="crfoota" href="#"><img class="crimgfoot1" src="images/logofacebook.jpg" alt="facebook"></a></li>
                 <li class="crfootli"><a class="crfoota"  href="#"><img class="crimgfoot1"  src="images/instagram.png" alt="instagram"></a></li>
                 <li class="crfootli"><a class="crfoota"  href="#"><img class="crimgfoot2" src="images/twitter.png" alt="twitter"></a></li>
@@ -17,7 +17,7 @@
                 
         <div class="crfootdiv2">
             <ul class="crfootul">
-                <li><h2 class="crfooth2">Nous Suivre</h2></li>
+                <li><h2 class="crfooth2">Contact</h2></li> <hr class="crfooterhr">
                 <li class="crfootli"><a class="crfoota"  href="#">label</a></li>
                 <li class="crfootli"><a class="crfoota"  href="#">devellopeur</a></li>
                 <li class="crfootli"><a class="crfoota"  href="#">presse</a></li>
@@ -26,7 +26,7 @@
 
         <div class="crfootdiv2">
             <ul class="crfootul">
-                <li><h2 class="crfooth2">Plan Du Site</h2></li>
+                <li><h2 class="crfooth2">Plan Du Site</h2></li> <hr class="crfooterhr">
                 <li class="crfootli"><a class="crfoota" href="admin.php">Admin</a></li>
                 <li class="crfootli"><a class="crfoota" href="article.php">Article</a></li>
                 <li class="crfootli"><a class="crfoota" href="connexion.php">connexion</a></li>
