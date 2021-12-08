78168d99425b151d461c9feaea38c6bcf63ff80c
Marc Keil <marc.keil@laplateforme.io>
2021-12-08 16:58:15 +0100

    Auto stash before rebase of "main"

--- créerarticle.php
+++ créerarticle.php
@@ -77,7 +77,7 @@ else
         <input type="text" placeholder="Titre" name="titre" id="titre" value="<?php if(isset($titre)) { echo $titre; } ?>" ><br><br>
         <select name="select" id="select">
       <?php while($lis = $listecate->fetch()) { ?>
-                    
+
                     <option><?= $lis['nom'] ?></option>
         <?php } ?>
           </select><br><br>
