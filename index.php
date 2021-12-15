b6fa7d71e4bd56ffe83a3df4da50cde9bbbb771e
rodriguez <claude.rodriguez@laplateforme.io>
2021-12-15 11:05:33 +0100

    frontv6

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ require_once("config/bdd.php");
 
 <head>
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bungee+Inline&family=Convergence&display=swap" rel="stylesheet">
     <title>Index</title>
 </head>
 
@@ -18,16 +21,32 @@ require_once("config/bdd.php");
         }
         ?>
     </header>
-    <main>
-        <h1>Alain</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quo, nostrum error fugiat architecto, nam ipsam explicabo animi rerum odio officia aliquam? Dolore eum deserunt ullam dolores odit voluptatum eos?</p>
-    </main>
-    <footer>
-        <?php
-        include_once('include/footer.php');
-        ?>
-    </footer>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Index</title>
+    </head>
 
-</body>
+    <body>
+        <header> <?php if (isset($_SESSION['login'])) {
+                        include_once("include/headerOnline.php");
+                    } else {
+                        include_once('include/header.php');
+                    } ?> </header>
+        <main id="crindexmain">
+            <div class="crindexdiv">
+                <h1 id="crindexh1" class="text-light">LE BLOG</h1>
+            </div>
+            <div class="crindexdiv">
+                <p id="crindexp" class="text-light"> c'est par ici <a href="article.php"><img id="crimgindexp" src="images/fleche.png" alt="facebook"></a></p>
+            </div>
+        </main>
+        <footer>
 
-</html>
+            <footer>
+                <?php
+                include_once('include/footer.php');
+                ?>
+            </footer>
+
+    </body>
+
+    </html>
