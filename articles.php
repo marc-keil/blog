9e16bb015bec327314e9121cd62ca99fdf3b7d5f
Alex-Zicaro <alex.zicaro@laplateforme.io>
2021-12-24 16:58:28 +0100

    cestbon

--- articles.php
+++ articles.php
@@ -8,24 +8,6 @@ if (isset($_GET["page"]) && !empty($_GET["page"])) {
     $currentPage = 1;
 }
 
-// on détermine le nombre total d'articles 
-$sql2 = "SELECT COUNT(*) AS `nb_articles` FROM `articles` ";
-// on prépare la requête
-$req2 = $bdd->prepare($sql2);
-// on éxécute la requête
-$req2->execute();
-// on récupère le nombre d'aritcle
-$result = $req2->fetch();
-$nbArticles = (int) $result["nb_articles"];
-
-// on limite par 5 le nb d'article par page
-$parPage = 5;
-//on calcule le nombre de page total
-$pages = ceil($nbArticles / $parPage);
-//var_dump($pages)
-
-//calcule du 1er article de la page
-$premier = ($currentPage * $parPage) - $parPage;
 
 
 
@@ -56,27 +38,57 @@ $premier = ($currentPage * $parPage) - $parPage;
 <body class="az">
     <header>
         <?php
+
         if (isset($_SESSION['login'])) { // si le gadjo est co 
             include_once("include/headeronline.php"); //tu mets 
 
         } else {
             include_once('include/header.php'); //sinon ça 
         }
-        if(isset($_GET["categorie"])){
+        if (isset($_GET["categorie"])) {
+
             $lacateg = $_GET['categorie'];
+
+            // on détermine le nombre total d'articles 
+            $sql2 = "SELECT COUNT(*) AS `nb_articles` FROM `articles`
+            INNER JOIN categories ON articles.id_categorie = categories.id
+            WHERE categories.id  = ?  ";
+            
+            // on prépare la requête
+            $req2 = $bdd->prepare($sql2);
+            
+            // on éxécute la requête
+            $req2->execute(array($lacateg));
+            // on récupère le nombre d'aritcle
+            $result = $req2->fetch();
+            $nbArticles = (int) $result["nb_articles"];
+
+            // on limite par 5 le nb d'article par page
+            $parPage = 5;
+            //on calcule le nombre de page total
+            $pages = ceil($nbArticles / $parPage);
+            //var_dump($pages)
+
+            //calcule du 1er article de la page
+            $premier = ($currentPage * $parPage) - $parPage;
+
+
+
+
+            
 
             $sql = "SELECT articles.id, articles.article, articles.id_utilisateur, articles.id_categorie, articles.date, articles.titre, utilisateurs.login 
             FROM `articles` 
             INNER JOIN categories ON articles.id_categorie = categories.id 
             INNER JOIN utilisateurs ON utilisateurs.id = articles.id_utilisateur 
-            WHERE categories.id = :id_categorie ORDER BY date DESC LIMIT :premier , :parpage; ";
+            WHERE categories.id = id_categorie ORDER BY date DESC LIMIT :premier , :parpage; ";
 
             $req = $bdd->prepare($sql);
             $req->bindValue(':premier', $premier, PDO::PARAM_INT);
-            $req->bindValue(':id_categorie', $lacateg, PDO::PARAM_INT);
+            
             $req->bindValue(':parpage', $parPage, PDO::PARAM_INT);
             $req->execute();
-            
+
             // on récupère toute les valeurs dans notre dictionnaire
             $articles = $req->fetchAll();
         ?>
@@ -120,109 +132,135 @@ $premier = ($currentPage * $parPage) - $parPage;
                     </article>
                 </section>
             <?php }
-            
-            
-            ?>
-                <nav>
-                    <ul class="pagination align-item-center">
-                        <li class="page-item <?= ($currentPage == 1) ? "disabled" : "" ?>">
-                            <a href="articles.php?page=<?= $currentPage - 1 ?>&categorie=<?= $lacateg ?>" class="page-link">Précédente</a>
-                        </li>
-                        <?php for ($page = 1; $page <= $pages; $page++) : ?>
-                            <li class="page-item <?= ($currentPage == $page) ? "active" : "" ?>">
-                                <a href="articles.php?page=<?= $page ?>&categorie=<?= $lacateg ?>" class="page-link"><?= $page ?></a>
-                            </li>
-                        <?php endfor; ?>
-                        <li class="page-item <?= ($currentPage == $pages) ? "disabled" : "" ?>">
-                            <a href="articles.php?page=<?= $currentPage + 1 ?>&categorie=<?= $lacateg ?>" class="page-link">Suivante</a>
-                        </li>
-                    </ul>
-                </nav>
 
-            <?php //sinon on  affiche cette pagination
-            } else {
-                $sql = "SELECT articles.id, articles.article, articles.id_utilisateur, articles.id_categorie, articles.date, articles.titre, utilisateurs.login 
+
+            ?>
+            <nav>
+                <ul class="pagination align-item-center">
+                    <li class="page-item <?= ($currentPage == 1) ? "disabled" : "" ?>">
+                        <a href="articles.php?page=<?= $currentPage - 1 ?>&categorie=<?= $lacateg ?>" class="page-link">Précédente</a>
+                    </li>
+                    <?php for ($page = 1; $page <= $pages; $page++) : ?>
+                        <li class="page-item <?= ($currentPage == $page) ? "active" : "" ?>">
+                            <a href="articles.php?page=<?= $page ?>&categorie=<?= $lacateg ?>" class="page-link"><?= $page ?></a>
+                        </li>
+                    <?php endfor; ?>
+                    <li class="page-item <?= ($currentPage == $pages) ? "disabled" : "" ?>">
+                        <a href="articles.php?page=<?= $currentPage + 1 ?>&categorie=<?= $lacateg ?>" class="page-link">Suivante</a>
+                    </li>
+                </ul>
+            </nav>
+
+        <?php //sinon on  affiche cette pagination
+        } else {
+
+
+            // on détermine le nombre total d'articles 
+            $sql2 = "SELECT COUNT(*) AS `nb_articles` FROM `articles` ";
+            // on prépare la requête
+            $req2 = $bdd->prepare($sql2);
+            // on éxécute la requête
+            $req2->execute();
+            // on récupère le nombre d'aritcle
+            $result = $req2->fetch();
+            $nbArticles = (int) $result["nb_articles"];
+
+            // on limite par 5 le nb d'article par page
+            $parPage = 5;
+            //on calcule le nombre de page total
+            $pages = ceil($nbArticles / $parPage);
+            //var_dump($pages)
+
+            //calcule du 1er article de la page
+            $premier = ($currentPage * $parPage) - $parPage;
+
+
+
+
+
+
+            $sql = "SELECT articles.id, articles.article, articles.id_utilisateur, articles.id_categorie, articles.date, articles.titre, utilisateurs.login, categories.nom
                 FROM `articles` 
                 INNER JOIN categories ON articles.id_categorie = categories.id 
                 INNER JOIN utilisateurs ON utilisateurs.id = articles.id_utilisateur 
-                WHERE categories.id = categories.id ORDER BY date DESC LIMIT :premier , :parpage; ";
-                
-                $req = $bdd->prepare($sql);
-                $req->bindValue(':premier', $premier, PDO::PARAM_INT);
-                // $req->bindValue(':id_categorie', $lacateg, PDO::PARAM_INT);
-                $req->bindValue(':parpage', $parPage, PDO::PARAM_INT);
-                $req->execute();
-                
-                // on récupère toute les valeurs dans notre dictionnaire
-                $articles = $req->fetchAll();
-            ?>
-        </header>
-        <div>
-            <main class="container">
-                <h1 class="text-light text-center">Listes des articles</h1>
-                <?php
-                foreach ($articles as $article) { ?>
-                    <section class="pt-5">
-                        <article class="d-flex flex-column ">
-    
-                            <h2 class="text-light text-center">
-                                <?php echo "titre : " . strip_tags($article["titre"]); ?>
-                            </h2>
-                            <h3 class="text-light text-center">
-                                <?php
-                                echo "catégorie : " . strip_tags($lacateg)
-                                ?>
-                            </h3>
-    
-    
-                            <div class="texte_article">
-                                <?php echo "article : " . "<br>" ?>
-                                <a class="ahref" href="article.php?article=<?= $article["id"]; ?>">
-                                    <?php echo strip_tags($article["article"]);  ?>
-                                </a>
-                            </div>
-                            <div class="text-light text-center">
-                                <p>
-                                    <?php echo  "publié le :" . " " . $article["date"]; ?>
-                                </p>
-                                <p>
-                                    <?php echo "par : " . $article["login"] ?>
-                            </div>
-    
-                            <hr class="text-light">
-    
-    
-    
-                        </article>
-                    </section>
-                <?php }
-                
-                
-                
-            ?>
-                <nav>
-                    <ul class="pagination align-item-center">
-                        <li class="page-item <?= ($currentPage == 1) ? "disabled" : "" ?>">
-                            <a href="articles.php?page=<?= $currentPage - 1 ?>" class="page-link">Précédente</a>
-                        </li>
-                        <?php for ($page = 1; $page <= $pages; $page++) : ?>
-                            <li class="page-item <?= ($currentPage == $page) ? "active" : "" ?>">
-                                <a href="articles.php?page=<?= $page ?>" class="page-link"><?= $page ?></a>
+                WHERE categories.id =  categories.id ORDER BY date DESC LIMIT :premier , :parpage; ";
+
+            $req = $bdd->prepare($sql);
+            $req->bindValue(':premier', $premier, PDO::PARAM_INT);
+            //$req->bindValue(':id_categorie', $lacateg, PDO::PARAM_INT);
+            $req->bindValue(':parpage', $parPage, PDO::PARAM_INT);
+            $req->execute();
+
+            // on récupère toute les valeurs dans notre dictionnaire
+            $articles = $req->fetchAll();
+        ?>
+            </header>
+            <div>
+                <main class="container">
+                    <h1 class="text-light text-center">Listes des articles</h1>
+                    <?php
+                    foreach ($articles as $article) { ?>
+                        <section class="pt-5">
+                            <article class="d-flex flex-column ">
+
+                                <h2 class="text-light text-center">
+                                    <?php echo "titre : " . strip_tags($article["titre"]); ?>
+                                </h2>
+                                <h3 class="text-light text-center">
+                                    <?php
+                                    echo "catégorie : " . strip_tags($article["nom"])
+                                    ?>
+                                </h3>
+
+
+                                <div class="texte_article">
+                                    <?php echo "article : " . "<br>" ?>
+                                    <a class="ahref" href="article.php?article=<?= $article["id"]; ?>">
+                                        <?php echo strip_tags($article["article"]);  ?>
+                                    </a>
+                                </div>
+                                <div class="text-light text-center">
+                                    <p>
+                                        <?php echo  "publié le :" . " " . $article["date"]; ?>
+                                    </p>
+                                    <p>
+                                        <?php echo "par : " . $article["login"] ?>
+                                </div>
+
+                                <hr class="text-light">
+
+
+
+                            </article>
+                        </section>
+                    <?php }
+
+
+
+                    ?>
+                    <nav>
+                        <ul class="pagination align-item-center">
+                            <li class="page-item <?= ($currentPage == 1) ? "disabled" : "" ?>">
+                                <a href="articles.php?page=<?= $currentPage - 1 ?>" class="page-link">Précédente</a>
                             </li>
-                        <?php endfor; ?>
-                        <li class="page-item <?= ($currentPage == $pages) ? "disabled" : "" ?>">
-                            <a href="articles.php?page=<?= $currentPage + 1 ?>" class="page-link">Suivante</a>
-                        </li>
-                    </ul>
-                </nav>
+                            <?php for ($page = 1; $page <= $pages; $page++) : ?>
+                                <li class="page-item <?= ($currentPage == $page) ? "active" : "" ?>">
+                                    <a href="articles.php?page=<?= $page ?>" class="page-link"><?= $page ?></a>
+                                </li>
+                            <?php endfor; ?>
+                            <li class="page-item <?= ($currentPage == $pages) ? "disabled" : "" ?>">
+                                <a href="articles.php?page=<?= $currentPage + 1 ?>" class="page-link">Suivante</a>
+                            </li>
+                        </ul>
+                    </nav>
 
-            <?php } ?>
+                <?php } ?>
 
-        </main>
-    </div>
-    <footer>
-        <?php include_once("include/footer.php");   ?>
-    </footer>
+                </main>
+            </div>
+            <footer>
+                <?php include_once("include/footer.php");   ?>
+            </footer>
 </body>
 
 </html>
