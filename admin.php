d9cf66f5127e9d8c66b442d7a1a034a5ae3a344f
Marc Keil <marc.keil@laplateforme.io>
2022-01-17 11:27:53 +0100

    salut c le novo blog reparé par bob

--- admin.php
+++ admin.php
@@ -134,73 +134,6 @@ if (isset($_POST['select'])) {
 }
 
 
-// Fonction modifié le titre
-if (isset($_POST['modiftitre']) && !empty($_POST['modiftitre'])) {
-    $idchange = $_POST['ida'];
-    $modificationtitre = $_POST['modiftitre'];
-    $requetetitre = $bdd->prepare("SELECT * FROM articles WHERE titre = ?"); // SAVOIR SI LE MEME LOGIN EST PRIS
-    $requetetitre->execute(array($modificationtitre));
-    $titreexist = $requetetitre->rowCount(); // rowCount = Si une ligne existe = PAS BON
-    var_dump($msg);
-
-    if ($titreexist !== 0) {
-        $_SESSION['msg'] = $_SESSION['msg'] . "Le titre éxiste déjà ! <br>";
-    } else {
-
-        $newtitre = htmlspecialchars($_POST['modiftitre']);
-        $insertnewtitre = $bdd->prepare("UPDATE articles SET titre = ? WHERE id = ?");
-        $insertnewtitre->execute(array($newtitre, $idchange));
-        header('Location: admin.php');
-        exit();
-    }
-}
-
-
-
-
-// Fonction modifié l'article
-if (isset($_POST['modifarticle']) && !empty($_POST['modifarticle'])) {
-    $idchange = $_POST['ida'];
-    $modificationarticle = $_POST['modifarticle'];
-    $requetearct = $bdd->prepare("SELECT * FROM articles WHERE article = ?"); // SAVOIR SI LE MEME LOGIN EST PRIS
-    $requetearct->execute(array($modificationarticle));
-    $articleexist = $requetearct->rowCount(); // rowCount = Si une ligne existe = PAS BON
-    var_dump($msg);
-
-    if ($articleexist !== 0) {
-        $_SESSION['msg'] = $_SESSION['msg'] . "Il n'y as pas eu de modifications sur l'article <br>";
-    } else {
-
-        $newarticle = htmlspecialchars($_POST['modifarticle']);
-        $insertnewarticle = $bdd->prepare("UPDATE articles SET article = ? WHERE id = ?");
-        $insertnewarticle->execute(array($newarticle, $idchange));
-        header('Location: admin.php');
-        exit();
-    }
-}
-
-
-// Fonction modifié la catégorie d'un article
-if (isset($_POST['selectc'])) {
-
-
-    $idchange = $_POST['ida'];
-    $categoriechange = $_POST['selectc'];
-    $changercateg = $bdd->prepare("UPDATE articles SET id_categorie = ? WHERE id = ?");
-    $changercateg->execute(array($categoriechange, $idchange));
-    header('Location: admin.php');
-    exit();
-}
-
-// Fonction supprimé un article
-if (isset($_GET['supprimerarticle']) && !empty($_GET['supprimerarticle'])) {
-    $supprimerarticle = (int) $_GET['supprimerarticle'];
-    $reqc = $bdd->prepare('DELETE FROM articles WHERE id = ?');
-    $reqc->execute(array($supprimerarticle));
-    header("Location: admin.php");
-    exit();
-}
-
 
 ?>
 
@@ -282,42 +215,8 @@ if (isset($_GET['supprimerarticle']) && !empty($_GET['supprimerarticle'])) {
                     <td class=test><input id="" type="submit" class="btn btn-primary mt-4 ms-3" name="submit" value="Confirmé !"></td>
                 </form>
             </table>
-            <table>
-                <thead>
-                    <tr class=test>
-                        <th class="text-light">Titre</th>
-                        <th class="text-light">Article</th>
-                        <th class="text-light">Catégorie</th>
-
-                    </tr>
-                </thead>
-                <?php while ($a = $listearticles->fetch()) { ?>
-                    <tr>
-                        <form  method="POST">
-                            <input id="ida" type="hidden" name="ida" value="<?php echo $a['ida']; ?>">
-                            <label class="text-light" for="modiftitre"></label>
-                            <td><input class="" id="modiftitre" type="text" name="modiftitre" value="<?php echo $a['titre']; ?>"></td>
-                            <label class="text-light" for="modifarticle"></label>
-                            <td><textarea class="" id="modifarticle" rows="5" cols="33" name="modifarticle"> <?= $a['article']; ?></textarea></td>
-                            <td>
-                                <select name="selectc" id="selectc">
-                                    <?php foreach ($fetchcate as $key => $value) { ?>
-                                        <option <?= $a['id_categorie'] == $value['idc'] ? "selected" : NULL ?> value="<?= $value['idc'] ?>"><?= $value['nom'] ?></option>
-                                    <?php
-
-                                    } ?>
-                                </select>
-                            </td>
-                            <td class=test><a class="btn btn-danger" href="admin.php?supprimerarticle=<?= $a['ida'] ?>">Supprimer l'article</a></td>
-                            <td class=test><input id="" type="submit" class="btn btn-primary" name="submit" value="Modifier"></td>
-                        </form>
-                    </tr>
-                <?php } ?>
-
-            </table>
             <br>
-
-            <br>
+            <a href="editionarticle.php"><input class="btn btn-secondary btn-lg" type="button" value="Modifier article"></a>
             <?php
 
             if (isset($_SESSION['msg'])) {
